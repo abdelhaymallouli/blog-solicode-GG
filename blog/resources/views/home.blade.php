d3200ab3adc3c078b108145b645e9ae721165ff6
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2025-12-25 09:02:51 +0000

    enhanced the blade template

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -60,7 +60,7 @@
 
         <!-- Hero Image / Illustration -->
         <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="{{ asset('images/banner.jpg') }}"
+            <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="{{ asset('images/banner2.jpg') }}"
                 alt="Solicode Banner">
         </div>
     </div>
